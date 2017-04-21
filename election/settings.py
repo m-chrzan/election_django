c865be23026b6e229e4d0f6c57ff1b873445af81
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-04-21 02:44:56 +0200

    Set up Jinja environment

--- election/settings.py
+++ election/settings.py
@@ -54,17 +54,10 @@ ROOT_URLCONF = 'election.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': ['templates'],
         'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+        'OPTIONS': { 'environment': 'election.jinja2.environment' },
     },
 ]
 
