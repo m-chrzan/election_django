efbf834d7638967a30bea57020eb9dbebc93d337
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-04-27 22:49:02 +0200

    Remove old circuit and candidate views

--- election2000/forms.py
+++ election2000/forms.py
@@ -2,16 +2,6 @@ from django import forms
 from election2000.models import Circuit, Votes
 from django.contrib.auth.models import User
 
-class CircuitForm(forms.ModelForm):
-    class Meta:
-        model = Circuit
-        fields = ['eligible', 'ballots_given_out', 'ballots_valid']
-
-class VotesForm(forms.ModelForm):
-    class Meta:
-        model = Votes
-        fields = ['number']
-
 class UserForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget = forms.PasswordInput)
--- election2000/views.py
+++ election2000/views.py
@@ -22,40 +22,31 @@ def gmina(request, voivodeship, district, gmina):
 
 @login_required
 def circuit(request, voivodeship, district, gmina, circuit):
-    candidates = Candidate.objects.all()
     circ = Circuit.objects.get(district__number = int(district),
             gmina__name = gmina, number = int(circuit))
+    votes = Votes.objects.filter(circuit = circ)
     if request.method == "POST":
-        form = CircuitForm(request.POST)
-        if form.is_valid():
-            save_circuit(circ, form.cleaned_data)
-            return HttpResponseRedirect('/Polska/' + '/'.join([voivodeship,
-                district, gmina]) + '/')
+        for vote in votes:
+            vote.number = int(request.POST.get(str(vote.pk)))
+            vote.save()
+        return HttpResponseRedirect('/Polska/' + '/'.join([voivodeship,
+            district, gmina]) + '/')
+
     else:
-        form = CircuitForm(instance = circ)
+        candidates = get_candidates(votes)
 
-    return render(request, 'circuit.html', {'form': form, 'circuit': circuit,
-        'candidates': candidates})
+    return render(request, 'circuit.html', {'candidates': candidates})
 
-@login_required
-def candidate(request, voivodeship, district, gmina, circuit, candidate):
-    first_name = ' '.join(candidate.split()[0:-1])
-    last_name = candidate.split()[-1]
-    votes = Votes.objects.get(candidate__first_name = first_name,
-            candidate__last_name = last_name, circuit__number = int(circuit),
-            circuit__gmina__name = gmina,
-            circuit__district__number = int(district))
-
-    if request.method == "POST":
-        form = VotesForm(request.POST)
-        if form.is_valid():
-            save_votes(votes, form.cleaned_data)
-            return HttpResponseRedirect('/Polska/' + '/'.join([voivodeship,
-                district, gmina]) + '/')
-    else:
-        form = VotesForm(instance = votes)
-
-    return render(request, 'votes.html', {'form': form, 'votes': votes})
+def get_candidates(votes):
+    candidates = []
+    for vote in votes:
+        candidates.append({
+            'first_name': vote.candidate.first_name,
+            'last_name': vote.candidate.last_name,
+            'id': vote.pk,
+            'votes': vote.number
+        })
+    return candidates
 
 def login_view(request):
     if request.method == "POST":
